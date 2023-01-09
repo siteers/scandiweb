96b2c277c1d04187ef92ed94d89061d225da7a47
Krzysztof Sitko <122014903+siteers@users.noreply.github.com>
2023-01-09 16:41:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="pl">
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>juniortest.Krzysztof.Sitko.com/</title>
+    <link rel='stylesheet' type='text/css' href='styles.css'/>
+    <style>
+.h1
+{
+position:absolute;
+right:10px;
+top:0px;
+}
+
+.h2
+{
+position:absolute;
+right:150px;
+top:0px;
+}
+
+.delete-checkbox
+{
+top: -30px;
+left:-100px;
+position: relative;
+}
+
+.container
+{
+width:200px;
+height:125px;
+background-color: rgb(168, 225, 185);
+float:left;
+border:2px solid black;
+padding:40px;
+margin:40px;
+text-align: center;
+font-size:18px;
+}
+
+.button {
+background-color: #4CAF50;
+border: none;
+color: white;
+padding: 1px 10px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 4px 2px;
+cursor: pointer;
+}
+</style>
+</head>
+
+<body>
+<h1><b>Product list</b><hr></h1>
+
+<form method="POST" action='index.php'>
+<a href='product-add.php'><h2 class='h2'><input type="button" name="button1" value="ADD" class='button'></h2></a>
+  
+<?php
+#PHP CODE SHOW ADDED DATA IN "PRODUCT-ADD.PHP" FROM MYSQL 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "test1";
+
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    if ($connection->connect_error) {
+        die("connectnio failed: ". $connection->connect_error);
+    }
+
+    $sql = "SELECT * FROM products";
+    $result = $connection->query($sql);
+    $query = mysqli_query($connection,$sql);
+
+    if (!$result){
+        die("invalid query: ". $connection->error);
+    }
+    while($row = mysqli_fetch_array($query)){
+        
+#this "if" causes the correct extensions to be displayed
+        if($row['weight']=="" && $row['size'] == ""){
+
+        echo "
+        <div class='container'><input type='checkbox' name='checkbox[]' class='delete-checkbox' value='".$row['id']."'
+        <b><br>$row[sku]<br>$row[name]<br>$row[price] $<br>Dimension: $row[height]x$row[width]x$row[lenght]</b></div>
+        ";}
+        elseif($row['height']=="" && $row['size']=="" && $row['width']=="" && $row['lenght']=="" ){
+
+        echo "
+        <div class='container'><input type='checkbox' name='checkbox[]' class='delete-checkbox' value='".$row['id']."'
+        <b><br>$row[sku]<br>$row[name]<br>$row[price] $<br>Weight: $row[weight] kg<br></b></div>
+        ";}
+        elseif($row['height']=="" && $row['weight']=="" && $row['width']=="" && $row['lenght']=="" ){
+
+        echo "
+        <div class='container'><input type='checkbox' name='checkbox[]' class='delete-checkbox' value='".$row['id']."'
+        <b><br>$row[sku]<br>$row[name]<br>$row[price] $<br>Size: $row[size] MB<br></b></div>
+        ";}    
+    }
+?>
+
+<h2 class='h1'><input type="submit" name="delete" value="MASS DELETE" id='delete-product-btn' class='button'></h2>  
+
+<?php
+
+#THIS PHP CODES CAUSES DELETE DATA FROM MYSQL SERVER AND INDEX.PHP
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "test1";
+
+    $connection = new mysqli($servername, $username, $password, $database);
+    if ($connection->connect_error) {
+        die("connectnio failed: ". $connection->connect_error);
+    }
+
+    if(isset($_POST['delete'])) {
+        $checkbox = $_POST['checkbox'];
+        foreach ($checkbox as $id) {
+        mysqli_query($connection,"DELETE FROM products WHERE id=".$id);}
+        header("location:index.php"); }
+        mysqli_close($connection);     
+?>   
+</form>
+        
+</body>
+</html>
